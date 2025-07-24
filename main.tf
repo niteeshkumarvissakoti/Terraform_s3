503d300a32123f80e791b393c8e9b9b7d331a2a9
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-24 18:11:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
+resource "aws_s3_bucket_ownership_controls" "oc" {
   bucket = aws_s3_bucket.mybucket.id
-
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
-resource "aws_s3_bucket_public_access_block" "example" {
+
+resource "aws_s3_bucket_public_access_block" "pb" {
   bucket = aws_s3_bucket.mybucket.id
   block_public_acls       = false
   block_public_policy     = false
@@ -34,15 +34,15 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "acl" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
+    aws_s3_bucket_ownership_controls.oc,
+    aws_s3_bucket_public_access_block.pb
   ]
-
   bucket = aws_s3_bucket.mybucket.id
   acl    = "public-read"
 }
+
 
 resource "aws_s3_object" "index" {
   bucket = "${aws_s3_bucket.mybucket.id}"
